5e5ce9b255c763902cde64d87f3e2455f754b0b2
Rcin <cindyhyf@qq.com>
2023-05-08 15:07:52 -0400

    updated distance calculation

--- fris/public/main-v2.js
+++ fris/public/main-v2.js
@@ -7,18 +7,21 @@ let camera, scene, renderer, controls, container, sound;
 
 let frisbee, frisbeeLoaded = false, throwFris = false, throwbackFris = false;
 
+let latitude, longitude;
+
 let socket = io();
 
 function init(){
     setupScene();
 
-    let blocker = document.getElementById( 'blocker' );
-    let instructions = document.getElementById( 'instructions' );
+    const blocker = document.getElementById( 'blocker' );
+    const instructions = document.getElementById( 'instructions' );
+    const popupbottom = document.getElementsByClassName("popupbottom")[1];
     blocker.addEventListener( 'click', function () {
         controls.lock();
         blocker.style.display = 'none';
-        document.getElementsByClassName("popup")[0].style.opacity = 100;
-        document.getElementById("instructions").style.opacity = 100;
+        popupbottom.style.opacity = 100;
+        instructions.style.opacity = 100;
         loadSound();
     });
 	controls.addEventListener( 'lock', function () {
@@ -28,13 +31,14 @@ function init(){
     
     controls.addEventListener( 'unlock', function () {
         blocker.style.display = 'block';
-        document.getElementsByClassName("popup")[0].style.opacity = 0;
+        popupbottom.style.opacity = 0;
         instructions.style.opacity = 0;
         console.log("unlocked");
     } );
     scene.add(controls.getObject());
     
     loadfrisbee();
+    getGeoLocation();  
     setupSocket();
     animate();
 }
@@ -124,7 +128,7 @@ function loadfrisbee(){
 function loadSound(){
     const listener = new THREE.AudioListener();
     const audioListenerMesh = new THREE.Mesh(
-        new THREE.BoxGeometry( 1, 1, 1 ),
+        new THREE.BoxGeometry( 0.1, 0.1, 0.1 ),
         new THREE.MeshBasicMaterial( { color: 'blue' } )
     );
     audioListenerMesh.add( listener );
@@ -132,7 +136,7 @@ function loadSound(){
     scene.add( audioListenerMesh );
     
     const mesh = new THREE.Mesh(
-        new THREE.SphereGeometry( 1, 32, 16 ),
+        new THREE.SphereGeometry( 0.1, 0.1, 0.1 ),
         new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } )
     );
     mesh.position.set(0, 5, 3);
@@ -180,19 +184,65 @@ function setupSocket(){
     let clientCounter = 0;
     socket.on("connect", () => {
         console.log("connected");
+
     });
     
-    socket.on("peerNumber", (peerCount) => {
-        console.log("current peer number", peerCount);
-        if (peerCount > 1){
-            console.log("more than 1 peer");
+    socket.on("peerID", (peer) => {
+        console.log("current peer IP", peer);
+        if (peer != null){
+            let peerLat = peer[0];
+            let peerLong = peer[1];
+            console.log("peerLat", peerLat, "peerLong", peerLong);
+        
+            let distance = calculateDistance(latitude, longitude, peerLat, peerLong);
+            console.log("distance", distance);
+            if (distance != null){
+            document.getElementById("ipaddress").innerHTML = distance+" km away";
+            // console.log(distance);
+            }
         }
+        
     });
 
     socket.on("throwback", (data) => {
-        console.log("throwback", data);
+        // console.log("throwback", data);
         throwbackFris = true;
     });
+}
+function calculateDistance(lat1,lon1,lat2,lon2){
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+}
+  
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
+}
+
+function getGeoLocation(){
+    function success(position) {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        console.log("Latitude is", latitude, "Longitude is", longitude);
+      }
+    
+      function error() {
+        console.log("Unable to retrieve your location");
+      }
+    
+      if (!navigator.geolocation) {
+        console.log("Geolocation is not supported by your browser");
+      } else {
+        navigator.geolocation.getCurrentPosition(success, error);
+      }
 }
 
 document.body.addEventListener( 'keydown', ( event ) => {
@@ -200,13 +250,29 @@ document.body.addEventListener( 'keydown', ( event ) => {
         console.log('throw');
         throwFris = true;
         socket.emit("throw", {id: socket.id, position: frisbee.position});
+        socket.emit("newPeer", {id: socket.id, latitude: latitude, longitude: longitude});
     }
 });
 
 let frameCount = 0;
 //animate
 function animate(){
-    let soundPlayed = false;
+    if (frisbee != null){
+    const getCameraRotate = new THREE.Vector3();
+    camera.getWorldDirection( getCameraRotate);
+    //  console.log(getCameraRotate);
+    getCameraRotate.y = 0;
+    frisbee.position.needsUpdate = true;
+    // console.log("current frisbee location", frisbee.position);
+    // frisbee.position.set(getCameraRotate.x * 0.5, 5, getCameraRotate.z * 0.5);
+    // frisbee.position.x += getCameraRotate.x * 0.1;
+    // frisbee.position.z += getCameraRotate.z * 0.1;
+    // frisbee.position.x.set(getCameraRotate.x * 0.5);
+    // frisbee.position.z.set(getCameraRotate.z * 0.5);
+    // getCameraRotate.add( frisbee.position );
+    // frisbee.position.add( getCameraRotate );
+    };
+
     if (frisbee != null && throwFris == true){
         frameCount++;
         // console.log("pre-throw", frisbee.position);
@@ -217,10 +283,6 @@ function animate(){
             sound.isPlaying = true;
             sound.play();
             console.log("sound played");
-            } else{
-                // sound.stop();
-                // sound.isPlaying = false;
-                console.log("sound stopped");
             }
         } else {
             throwFris = false;
@@ -230,23 +292,26 @@ function animate(){
             console.log("done throwing");
         }
 
-        frisbee.position.needsUpdate = true;
+        
     }
     if (frisbee != null && throwbackFris == true){
+        document.getElementsByClassName("popuptop")[0].style.opacity = 100;
+        document.getElementById("newconnection").style.opacity = 100;
         frameCount++;
         if(frisbee.position.z <= 3){
             frisbee.position.z += frameCount*0.005;
             frisbee.rotation.y -= frameCount*0.06;
+
             if (sound.isPlaying == false){
             sound.play();
-            }else{
-                sound.stop();
             }
             // console.log("throwing back")
         } else {
             throwbackFris = false;
             frameCount = 0;
             console.log("done throwing back");
+            document.getElementsByClassName("popuptop")[0].style.opacity = 0;
+            document.getElementById("newconnection").style.opacity = 0;
             // soundPlayed = false;  
         }
     } 
--- fris/server.js
+++ fris/server.js
@@ -20,33 +20,35 @@ io.on("connection", (socket) => {
     "Someone joined our server using socket.io.  Their socket id is",
     socket.id
   );
+  var address = socket.request.connection.remoteAddress;
+  if (address.match(/^::ffff:/)) {
+    address = address.replace(/^::ffff:/, "");
+    console.log("address", address);
+  }
   socket.emit("peerNumber", io.engine.clientsCount);
-  peers[socket.id] = {};
+  peers[socket.id] = address;
 
-  console.log("Current peers:", peers);
 
-  socket.on("newPeer", (client) => {
-    clientCounter = client;
-    console.log(io.engine.clientsCount);
-    // console.log("current peer number", clientCounter);
-    // socket.emit("currentPeer", clientCounter);
+  socket.on("newPeer", (location) => {
+    // console.log(io.engine.clientsCount);
+    console.log("peer location", location);
+    peers[location.id] = [location.latitude, location.longitude];
+    console.log("Current peers:", peers);
   });
 
   socket.on("throw", (data) => {
     console.log("throw", data);
+    let currentPeer = peers[data.id];
+    console.log("currentPeer", currentPeer);
     if (io.engine.clientsCount>1){
       setTimeout(function () { 
         socket.broadcast.emit("throwback", data);
+        socket.broadcast.emit("peerID", currentPeer);
         console.log("throwing back");
       }, 1000);
     }
   });
 
-  socket.on("msg", (data) => {
-    console.log("Got message from client with id ", socket.id, ":", data);
-    let messageWithId = { from: socket.id, data: data };
-    socket.broadcast.emit("msg", messageWithId);
-  });
 
   socket.on("disconnect", () => {
     console.log("Someone with ID", socket.id, "left the server");
